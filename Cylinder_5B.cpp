9f28255b8eb9b96e84381f28c90970330ceb3a09
zizifn <515868058@qq.com>
2014-11-21 18:20:28 +0800

    Create Cylinder_5B.cpp

---
+++ Cylinder_5B.cpp
@@ -0,0 +1,68 @@
+
+// you may need to remove ""stdafx.h""
+#include "stdafx.h"
+#include "Cylinder.h"
+#include <math.h>
+
+#define M_PI       3.14159265358979323846
+
+Cylinder::Cylinder()
+{
+	height = 1.0;
+	radius = 1.0;
+}
+Cylinder::Cylinder(double height,double radius)
+{
+	this->height = height;
+	this->radius = radius;
+}
+double Cylinder::get_height()
+{
+	return height;
+}
+double Cylinder::get_radius()
+{
+	return radius;
+}
+void Cylinder::get_values(double& h, double& r)
+{
+	h=height;
+	r=radius;
+}
+double Cylinder::volume()
+{
+	// you may need using math.h M_PI instead of 3.14
+	return M_PI*radius*radius*height;
+}
+void Cylinder::scale(double s)
+{
+	height*=s;
+	radius*=s;
+}
+void Cylinder::scale(double sh,double sr)
+{
+	height*=sh;
+	radius*=sr;
+}
+void Cylinder::print(ostream& out)
+{
+	out<<"Cylinder height: "<<height<<"   radius: "<<radius<<endl;
+
+}
+
+// those for HW05 B
+void Cylinder::operator*=(double scalar)
+{
+	this->scale(scalar);
+}
+
+std::ostream& operator<<(std::ostream  &out, Cylinder& other)
+{
+	out<<"Cylinder height: "<<other.get_height()<<" radius: "<<other.get_radius()<<endl;
+	return out;
+}
+
+Cylinder operator+(Cylinder c1,Cylinder c2)
+{
+	return Cylinder(c1.height+c2.height,(c1.radius+c2.radius)/2);
+}
